b798c55c588a7a3734b54273730756afc1e24bb7
andrew-lange13 <andrew.lange13@gmail.com>
2022-05-03 10:13:32 -0500

    adds user registration mappings

--- src/main/java/com/capstone/recycode/Controllers/UserController.java
+++ src/main/java/com/capstone/recycode/Controllers/UserController.java
@@ -1,15 +1,44 @@
 package com.capstone.recycode.Controllers;
 
+import com.capstone.recycode.Models.User;
+import com.capstone.recycode.Repositories.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+@Controller
 public class UserController {
 
-    @GetMapping("/home")
-    @ResponseBody
-    public String homePage() {
+    private UserRepository userDao;
 
-        System.out.println("endpoint");
-        return "This is the landing page!";
+    private PasswordEncoder passwordEncoder;
+
+    public UserController(UserRepository userDao, PasswordEncoder passwordEncoder){
+        this.userDao = userDao;
+        this.passwordEncoder = passwordEncoder;
     }
+
+    @GetMapping("/register")
+    public String showSignUpForm(Model model){
+        model.addAttribute("user", new User());
+        return "/users/register";
+    }
+
+    @PostMapping("/register")
+    public String saveUser(@ModelAttribute User user){
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
+        userDao.save(user);
+        return "redirect:/login";
+    }
+//    @GetMapping("/home")
+//    @ResponseBody
+//    public String homePage() {
+//
+//        System.out.println("endpoint");
+//        return "This is the landing page!";
+//    }
 }
