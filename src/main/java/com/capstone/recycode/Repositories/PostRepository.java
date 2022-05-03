9c04c7ba423d8993cf6af854dabe76585ce4739c
Mario Granados <mario.alberto.granados1@gmail.com>
2022-05-03 11:21:51 -0500

    model changes and post

--- src/main/java/com/capstone/recycode/Repositories/PostRepository.java
+++ src/main/java/com/capstone/recycode/Repositories/PostRepository.java
@@ -3,5 +3,5 @@ package com.capstone.recycode.Repositories;
 import com.capstone.recycode.Models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
