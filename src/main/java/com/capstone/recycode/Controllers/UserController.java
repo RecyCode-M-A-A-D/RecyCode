928ace2f3754b4fcff2b043817b837db4663a48a
Mario Granados <mario.alberto.granados1@gmail.com>
2022-05-05 11:15:54 -0500

    validation to register

--- src/main/java/com/capstone/recycode/Controllers/UserController.java
+++ src/main/java/com/capstone/recycode/Controllers/UserController.java
@@ -5,10 +5,8 @@ import com.capstone.recycode.Repositories.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
 @Controller
 public class UserController {
 
@@ -16,25 +14,46 @@ public class UserController {
 
     private PasswordEncoder passwordEncoder;
 
-    public UserController(UserRepository userDao, PasswordEncoder passwordEncoder){
+    public UserController(UserRepository userDao, PasswordEncoder passwordEncoder) {
         this.userDao = userDao;
         this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("/register")
-    public String showSignUpForm(Model model){
+    public String showSignUpForm(Model model) {
         model.addAttribute("user", new User());
         return "/users/register";
     }
 
     @PostMapping("/register")
-    public String saveUser(@ModelAttribute User user){
-        String hash = passwordEncoder.encode(user.getPassword());
-        user.setPassword(hash);
-        userDao.save(user);
-        return "redirect:/login";
-    }
+    public String saveUser(@RequestParam(name = "password") String password,
+                           @RequestParam(name = "confirm_password") String confirmPass,
+                           Model model,
+                           @ModelAttribute User user) {
+        //check if user already exsists
+        if (userDao.findUserByUserName(user.getUserName()) == null || userDao.findUserByEmail(user.getEmail()) == null) {
 
+            if (password.equals(confirmPass)) {
+                if (password.length() > 8) {
+                    model.addAttribute("user", user);
+                    String hash = passwordEncoder.encode(user.getPassword());
+                    user.setPassword(hash);
+                    userDao.save(user);
+                    return "redirect:/login";
+                } else {
+                    model.addAttribute("error", "Password must be 8 characters or more");
+                    return "/users/register";
+                }
+            } else {
+                model.addAttribute("error", "Passwords do not match");
+                return "/users/register";
+            }
+        } else {
+            model.addAttribute("error", "Username or email already taken");
+            return "users/register";
+        }
+
+    }
 
 
 }
--- src/main/java/com/capstone/recycode/Repositories/UserRepository.java
+++ src/main/java/com/capstone/recycode/Repositories/UserRepository.java
@@ -2,8 +2,15 @@ package com.capstone.recycode.Repositories;
 
 import com.capstone.recycode.Models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Query(value = "SELECT * FROM users WHERE email = ?1 LIMIT 1", nativeQuery = true)
+    User findUserByEmail(@RequestParam(name = "email") String email);
+
+    @Query(value = "SELECT * FROM users WHERE user_name = ?1 LIMIT 1", nativeQuery = true)
+    User findUserByUserName(@RequestParam(name ="user_name") String username);
 
     User findByUserName(String username);
 }
