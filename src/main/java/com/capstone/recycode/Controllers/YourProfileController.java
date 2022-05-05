00244536e3a0812c6128cb6e797c3a072835c9a8
Mario Granados <mario.alberto.granados1@gmail.com>
2022-05-05 09:43:51 -0500

    testing profile controller

--- src/main/java/com/capstone/recycode/Controllers/YourProfileController.java
+++ src/main/java/com/capstone/recycode/Controllers/YourProfileController.java
@@ -1,13 +1,26 @@
 package com.capstone.recycode.Controllers;
 
+import com.capstone.recycode.Models.User;
+import com.capstone.recycode.Repositories.PostRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class YourProfileController {
+    private PostRepository postDao;
+
+    public YourProfileController(PostRepository postDao) {
+        this.postDao = postDao;
+    }
 
     @GetMapping("/profile")
-    public String showProfile(){
+    public String showProfile(Model model) {
+        System.out.println("test");
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        System.out.println(user.getUserName());
+        model.addAttribute("posts", postDao.findPostsByUserId(user.getId()));
         return "/profile";
     }
 }
--- src/main/java/com/capstone/recycode/Repositories/PostRepository.java
+++ src/main/java/com/capstone/recycode/Repositories/PostRepository.java
@@ -2,8 +2,13 @@ package com.capstone.recycode.Repositories;
 
 import com.capstone.recycode.Models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-
+    @Query(value = "SELECT * FROM posts WHERE user_id = ?1", nativeQuery = true)
+    List<Post> findPostsByUserId(@RequestParam(name = "user_id") long id);
     Post findPostsByUser(String userName);
 }
