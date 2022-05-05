4f796edcfdb439b394449578c4bedc02cf82d975
Mario Granados <mario.alberto.granados1@gmail.com>
2022-05-05 15:47:34 -0500

    post_stats connection

--- src/main/java/com/capstone/recycode/Models/PostStat.java
+++ src/main/java/com/capstone/recycode/Models/PostStat.java
@@ -18,4 +18,43 @@ public class PostStat {
     @OneToOne
     Post post;
 
+    public PostStat() {}
+
+    public PostStat(long upVotesCount, long downVotesCount, Post post) {
+        this.upVotesCount = upVotesCount;
+        this.downVotesCount = downVotesCount;
+        this.post = post;
+    }
+
+    public long getStatId() {
+        return statId;
+    }
+
+    public void setStatId(long statId) {
+        this.statId = statId;
+    }
+
+    public long getUpVotesCount() {
+        return upVotesCount;
+    }
+
+    public void setUpVotesCount(long upVotesCount) {
+        this.upVotesCount = upVotesCount;
+    }
+
+    public long getDownVotesCount() {
+        return downVotesCount;
+    }
+
+    public void setDownVotesCount(long downVotesCount) {
+        this.downVotesCount = downVotesCount;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
 }
--- src/main/java/com/capstone/recycode/Repositories/PostStatRepository.java
+++ src/main/java/com/capstone/recycode/Repositories/PostStatRepository.java
@@ -2,6 +2,12 @@ package com.capstone.recycode.Repositories;
 
 import com.capstone.recycode.Models.PostStat;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 public interface PostStatRepository extends JpaRepository<PostStat, Long> {
+    @Query(value = "SELECT * FROM post_stats WHERE post_post_id = ?1", nativeQuery = true)
+    List<PostStat> findAllByPostId(@RequestParam(name = "post_post_id") Long id);
 }
