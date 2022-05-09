84c43ceabac7236857b9997777df2e95694b106f
andrew-lange13 <andrew.lange13@gmail.com>
2022-05-09 15:24:58 -0500

    matches profile pages

--- src/main/java/com/capstone/recycode/Controllers/YourProfileController.java
+++ src/main/java/com/capstone/recycode/Controllers/YourProfileController.java
@@ -1,9 +1,12 @@
 package com.capstone.recycode.Controllers;
 import com.capstone.recycode.Models.Favorite;
 import com.capstone.recycode.Models.Post;
+import com.capstone.recycode.Models.PostStat;
 import com.capstone.recycode.Models.User;
 import com.capstone.recycode.Repositories.FavoritesRepository;
 import com.capstone.recycode.Repositories.PostRepository;
+import com.capstone.recycode.Repositories.PostStatRepository;
+import com.capstone.recycode.Repositories.UserRepository;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,11 +20,19 @@ import java.util.List;
 @Controller
 public class YourProfileController {
     private PostRepository postDao;
+    private UserRepository userDao;
+    private PostStatRepository postStatDao;
     private FavoritesRepository favDao;
 
-    public YourProfileController(PostRepository postDao, FavoritesRepository favDao) {
-        this.favDao = favDao;
+    public YourProfileController(PostRepository postDao,
+                                 UserRepository userDao,
+                                 PostStatRepository postStatDao,
+                                 FavoritesRepository favDao) {
         this.postDao = postDao;
+        this.userDao = userDao;
+        this.postStatDao = postStatDao;
+        this.favDao = favDao;
+
     }
 
     @GetMapping("/profile")
@@ -30,11 +41,25 @@ public class YourProfileController {
         List<Favorite> favorites = favDao.findFavoritesByUserId(user.getId());
         List<Post> favoritePosts = new ArrayList<>();
 
-        for(int i = 0; i < favorites.size(); i++) {
+        List<Post> posts = postDao.findPostsByUserId(user.getId());
+        model.addAttribute("post", postDao.findPostsByUserId(user.getId()));
+        List<PostStat> postStats = new ArrayList<>();
+
+        for (int i = 0; i < posts.size(); i++) {
+            System.out.println("test");
+            postStats.add(postStatDao.findPostStatById(posts.get(i).getPostId()));
+        }
+
+
+        for (int i = 0; i < favorites.size(); i++) {
             favoritePosts.add(favorites.get(i).getPost());
         }
 
-        model.addAttribute("posts", postDao.findPostsByUserId(user.getId()));
+        for (int i = 0; i < favorites.size(); i++) {
+            favoritePosts.add(favorites.get(i).getPost());
+        }
+
+        model.addAttribute("postStats", postStats);
         model.addAttribute("favorites", favoritePosts);
         return "/profile";
     }
