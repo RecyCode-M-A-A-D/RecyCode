fa31322715dc055deb282db76990464c2e8883e5
andrew-lange13 <andrew.lange13@gmail.com>
2022-05-10 13:50:51 -0500

    filestack picker inline

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -2,10 +2,12 @@ window.addEventListener('DOMContentLoaded', function () {
     const apikey = key;
     const client = filestack.init(apikey);
     const options = {
+        displayMode: 'inline',
+        container: '#inline',
         maxFiles: 20,
         uploadInBackground: false,
-        onOpen: () => console.log('opened!'),
         onUploadDone: (res) => console.log(res),
     };
     client.picker(options).open();
+
 });
